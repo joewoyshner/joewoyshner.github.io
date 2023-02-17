4312d401fee223060b031f33dce59a2350b99584
Joseph Woyshner <joewoyshner@gmail.com>
2023-02-16 22:49:07 -0500

    include ngp

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'site';
+export class AppComponent implements OnInit {
+  title = 'jw';
+  events1!: any[];
+    
+  events2!: any[];
+  
+  ngOnInit() {
+      this.events1 = [
+          {status: 'Ordered', date: '15/10/2020 10:30', icon: PrimeIcons.CHECK_SQUARE, color: '#FF9800'},
+          {status: 'Processing', date: '15/10/2020 14:00', icon: PrimeIcons.CHECK_SQUARE, color: '#FF9800'},
+          {status: 'Shipped', date: '15/10/2020 16:15', icon: PrimeIcons.CHECK_SQUARE, color: '#FF9800'},
+          {status: 'Delivered', date: '16/10/2020 10:00', icon: PrimeIcons.CHECK_SQUARE, color: '#FF9800'}
+      ];
+
+      this.events2 = [
+          "2020", "2021", "2022", "2023"
+      ];
+  }
 }
--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TimelineModule } from 'primeng/timeline';
 import { AppComponent } from './app.component';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TimelineModule,
+    CardModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
